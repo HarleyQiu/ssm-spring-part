29e17e88d384b13ad83bc7769ef0e34ad287cc35
QiuLinhao <210554114@qq.com>
2023-09-12 17:26:23 +0800

    基于xml管理bean的方式 ioc容器的使用

--- spring-ioc-xml-01/src/test/java/org/example/test/SpringIocTest.java
+++ spring-ioc-xml-01/src/test/java/org/example/test/SpringIocTest.java
@@ -1,5 +1,7 @@
 package org.example.test;
 
+import org.example.ioc_03.HappyComponent;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringIocTest {
@@ -21,7 +23,24 @@ public class SpringIocTest {
     /**
      * 讲解如何在Ioc容器中获取组件Bean
      */
+    @Test
     public void getBeanFromIoc() {
+        // 方式一
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-03.xml");
+        // 1. 通过getBean方法获取组件Bean 根据Bean 返回值为Object 不推荐
+        HappyComponent happyComponent = (HappyComponent) applicationContext.getBean("happyComponent");
+
+        // 2. 通过getBean方法获取组件Bean 根据Bean和类型
+        HappyComponent happyComponent1 = applicationContext.getBean("happyComponent", HappyComponent.class);
+
+        // 3. 通过getBean方法获取组件Bean 根据类型 但是要求容器中只能有一个该类型的Bean
+        HappyComponent happyComponent2 = applicationContext.getBean(HappyComponent.class);
+
+        happyComponent.doWork();
+        happyComponent1.doWork();
+        happyComponent2.doWork();
+        System.out.println(happyComponent == happyComponent1);
+        System.out.println(happyComponent1 == happyComponent2);
 
     }
 }
