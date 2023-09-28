6a7fb3bde0e2e1a3e58d59b40c243cf597a03c8d
QiuLinhao <210554114@qq.com>
2023-09-28 19:59:53 +0800

    增加了事务 针对事务传播行为

--- spring-tx-annotation-10/src/main/java/org/example/service/StudentService.java
+++ spring-tx-annotation-10/src/main/java/org/example/service/StudentService.java
@@ -3,6 +3,7 @@ package org.example.service;
 import org.example.dao.StudentDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.FileInputStream;
@@ -14,6 +15,14 @@ import java.io.FileNotFoundException;
  * 3. rollbackFor = 指定哪些异常才会回滚,默认是 RuntimeException and Error 异常方可回滚
  * 4. noRollbackFor = 指定哪些异常不会回滚, 默认没有指定,如果指定,应该在rollbackFor的范围内
  * 5. isolation = 设置事务的隔离级别,mysql默认是repeatable read!
+ * 6. 其他传播行为值（了解）
+ *  6-1. Propagation.REQUIRED：如果当前存在事务，则加入当前事务，否则创建一个新事务。   默认值
+ *  6-2. Propagation.REQUIRES_NEW：创建一个新事务，并在新事务中执行。如果当前存在事务，则挂起当前事务，即使新事务抛出异常，也不会影响当前事务。
+ *  6-3. Propagation.NESTED：如果当前存在事务，则在该事务中嵌套一个新事务，如果没有事务，则与Propagation.REQUIRED一样。
+ *  6-4. Propagation.SUPPORTS：如果当前存在事务，则加入该事务，否则以非事务方式执行。
+ *  6-5. Propagation.NOT_SUPPORTED：以非事务方式执行，如果当前存在事务，挂起该事务。
+ *  6-6. Propagation.MANDATORY：必须在一个已有的事务中执行，否则抛出异常。
+ *  6-7. Propagation.NEVER：必须在没有事务的情况下执行，否则抛出异常。
  */
 @Service
 @Transactional(timeout = 3, rollbackFor = Exception.class, noRollbackFor = FileNotFoundException.class)
@@ -34,6 +43,20 @@ public class StudentService {
     @Transactional(readOnly = true)
     public String getEmpName(Integer empId) {
         return "";
+    }
+
+    /**
+     * 声明两个独立修改数据库的事务业务方法
+     */
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void changeAge() {
+        studentDao.updateAgeById(100, 1);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void changeName() {
+        studentDao.updateNameById("test3", 1);
+        int i = 1 / 0;
     }
 
 
--- spring-tx-annotation-10/src/test/java/org/example/test/SpringTxTest.java
+++ spring-tx-annotation-10/src/test/java/org/example/test/SpringTxTest.java
@@ -2,6 +2,7 @@ package org.example.test;
 
 import org.example.config.JavaConfig;
 import org.example.service.StudentService;
+import org.example.service.TopService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
@@ -13,9 +14,16 @@ public class SpringTxTest {
 
     @Autowired
     private StudentService studentService;
+    @Autowired
+    private TopService topService;
 
     @Test
-    public void testTx() throws FileNotFoundException {
+    public void test() throws FileNotFoundException {
         studentService.changeInfo();
+    }
+
+    @Test
+    public void testTx() {
+        topService.topService();
     }
 }
