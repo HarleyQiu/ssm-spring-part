798f9543bc51355019b7ea57aa20ef765120a0fa
QiuLinhao <210554114@qq.com>
2023-09-28 17:47:43 +0800

    修改了一些aop提示性代码

--- spring-aop-annotation-09/src/main/java/org/example/advice/TxAdvice.java
+++ spring-aop-annotation-09/src/main/java/org/example/advice/TxAdvice.java
@@ -9,16 +9,16 @@ public class TxAdvice {
 
     @Before("org.example.pointcut.MyPointcut.myPc()")
     public void begin() {
-        System.out.println("开启事务");
+        System.out.println("开启事务1");
     }
 
     @AfterReturning("org.example.pointcut.MyPointcut.myPc()")
     public void commit() {
-        System.out.println("事务提交");
+        System.out.println("提交事务1");
     }
 
     @AfterThrowing("org.example.pointcut.MyPointcut.myPc()")
     public void rollback() {
-        System.out.println("事务回滚");
+        System.out.println("事务回滚1");
     }
 }
